c28e452725a2f0e65a1a938326278928d9099fff
Daniel Cole <daniel@dancole.fr>
2018-04-18 10:52:40 +0200

    Add tests, travis & includeMessage option

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 A library to convert Objection errors to Boom errors
 
-***THIS IS A WORK IN PROGRESSS***
+[![Build Status](https://travis-ci.org/PixulHQ/avocat.svg?branch=master)](https://travis-ci.org/PixulHQ/avocat) [![Coverage Status](https://coveralls.io/repos/github/PixulHQ/avocat/badge.svg?branch=master)](https://coveralls.io/github/PixulHQ/avocat?branch=master) [![NSP Status](https://nodesecurity.io/orgs/pixulhq/projects/ed277a5f-854b-40f1-8935-e87a94d0f87f/badge)](https://nodesecurity.io/orgs/pixulhq/projects/ed277a5f-854b-40f1-8935-e87a94d0f87f)
+
+Lead Maintainer: [Daniel Cole](https://github.com/optii)
 
 
 ## Usage
@@ -14,6 +16,7 @@ A library to convert Objection errors to Boom errors
  - `error`: - the `Objection` error (any other types of errors are just ignored)
  - `options`: - optional object where:
      - `return`: - if `true` will return the error instead of throwing it
+     - `includeMessage`: if `true` the Boom error created will have it's message set to the message of the error thrown
 
 
 Heavily inspired by [Bounce](https://github.com/hapijs/bounce)
--- test/index.js
+++ test/index.js
@@ -5,16 +5,22 @@ const Lab  = require('lab');
 
 const lab = exports.lab = Lab.script();
 const { describe, it } = lab;
-const { expect }                      = Code;
+const { expect }       = Code;
 
-const Avocat = require('../lib');
+const { UniqueViolationError, DBError, CheckViolationError, DataError, ForeignKeyViolationError, ConstraintViolationError, NotNullViolationError } = require('db-errors');
+
+const Objection = require('objection');
+const Avocat    = require('../lib');
 
 describe('Avocat', () => {
 
     describe('rethrow', () => {
 
-        it('should return the error', () => {});
-        it('should throw the error', () => {});
+        it('should return false if not an error', () => {
+
+            expect(Avocat.rethrow('notanerror', { return : true })).to.equal(false);
+            expect(Avocat.rethrow('notanerror')).to.equal(false);
+        });
 
         it('should ignore non objection errors', () => {
 
@@ -23,12 +29,399 @@ describe('Avocat', () => {
                 Avocat.rethrow(new Error());
             }).to.not.throw();
 
-            expect(Avocat.rethrow(new Error(), { return: true })).to.equal(false);
+            expect(Avocat.rethrow(new Error(), { return : true })).to.equal(false);
         });
 
-        it('should throw notFound', () => {});
-        it('should throw badRequest', () => {});
-        it('should throw conflict', () => {});
-        it('should throw internal', () => {});
+        it('should throw the error', () => {
+
+            expect(() => {
+
+                Avocat.rethrow(new DBError());
+            }).to.throw();
+        });
+
+        it('should include error message', () => {
+
+            try {
+                Avocat.rethrow(new DBError({
+                    nativeError : {
+                        message : 'some native error message'
+                    }
+                }), { includeMessage : true });
+            }
+            catch (error) {
+
+                expect(error.message).to.equal('some native error message');
+            }
+
+        });
+
+        describe('Objection NotFoundError', () => {
+
+            it('should throw notFound', () => {
+
+                try {
+                    Avocat.rethrow(new Objection.NotFoundError());
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('notFound');
+                    expect(error.output.statusCode).to.equal(404);
+                }
+            });
+
+            it('should return notFound', () => {
+
+                const error = Avocat.rethrow(new Objection.NotFoundError(), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('notFound');
+                expect(error.output.statusCode).to.equal(404);
+            });
+
+            it('should include message', () => {
+
+                expect(Avocat.rethrow(new Objection.NotFoundError(), { return : true, includeMessage : true }).message).to.equal('NotFoundError');
+
+                try {
+                    Avocat.rethrow(new Objection.NotFoundError(), { return : true, includeMessage : true });
+                }
+                catch (error) {
+                    expect(error.message).to.equal('NotFoundError');
+                }
+
+            });
+        });
+
+        describe('Objection ValidationError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+                    Avocat.rethrow(new Objection.ValidationError({
+                        message : 'validation error',
+                        type    : 'ModelValidation',
+                        data    : 'some random data'
+                    }));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new Objection.ValidationError({
+                    message : 'validation error',
+                    type    : 'ModelValidation',
+                    data    : 'some random data'
+                }), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+
+            });
+
+            it('should include message', () => {
+
+                expect(Avocat.rethrow(new Objection.ValidationError({
+                    message : 'validation error',
+                    type    : 'ModelValidation',
+                    data    : 'some random data'
+                }), { return : true, includeMessage : true }).message).to.equal('validation error');
+
+                try {
+                    Avocat.rethrow(new Objection.ValidationError({
+                        message : 'validation error',
+                        type    : 'ModelValidation',
+                        data    : 'some random data'
+                    }), { return : true, includeMessage : true });
+                }
+                catch (error) {
+                    expect(error.message).to.equal('validation error');
+                }
+
+            });
+        });
+
+        describe('NotNullViolationError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+
+                    Avocat.rethrow(new NotNullViolationError(
+                        {
+                            table       : 'table',
+                            constraint  : 'some_contraint',
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new NotNullViolationError(
+                    {
+                        table       : 'table',
+                        constraint  : 'some_contraint',
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+
+            });
+        });
+
+        describe('ConstraintViolationError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+
+                    Avocat.rethrow(new ConstraintViolationError(
+                        {
+                            table       : 'table',
+                            constraint  : 'some_contraint',
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new ConstraintViolationError(
+                    {
+                        table       : 'table',
+                        constraint  : 'some_contraint',
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+            });
+        });
+
+        describe('ForeignKeyViolationError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+
+                    Avocat.rethrow(new ForeignKeyViolationError(
+                        {
+                            table       : 'table',
+                            constraint  : 'some_contraint',
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new ForeignKeyViolationError(
+                    {
+                        table       : 'table',
+                        constraint  : 'some_contraint',
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+
+            });
+
+        });
+
+        describe('DataError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+
+                    Avocat.rethrow(new DataError(
+                        {
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new DataError(
+                    {
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+
+            });
+        });
+
+        describe('CheckViolationError', () => {
+
+            it('should throw badRequest', () => {
+
+                try {
+                    Avocat.rethrow(new CheckViolationError(
+                        {
+                            table       : 'table',
+                            constraint  : 'some_contraint',
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('badRequest');
+                    expect(error.output.statusCode).to.equal(400);
+                }
+            });
+
+            it('should return badRequest', () => {
+
+                const error = Avocat.rethrow(new CheckViolationError(
+                    {
+                        table       : 'table',
+                        constraint  : 'some_contraint',
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('badRequest');
+                expect(error.output.statusCode).to.equal(400);
+
+            });
+        });
+
+        describe('UniqueViolationError', () => {
+
+            it('should throw conflict', () => {
+
+                try {
+                    Avocat.rethrow(new UniqueViolationError({
+                        table       : 'table',
+                        columns     : 'test',
+                        constraint  : 'some_contraint',
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('conflict');
+                    expect(error.output.statusCode).to.equal(409);
+                }
+            });
+
+            it('should return conflict', () => {
+
+                const error = Avocat.rethrow(new UniqueViolationError({
+                    table       : 'table',
+                    columns     : 'test',
+                    constraint  : 'some_contraint',
+                    nativeError : 'NATIVE ERROR',
+                    client      : 'CLIENT'
+                }), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('conflict');
+                expect(error.output.statusCode).to.equal(409);
+            });
+        });
+
+        describe('DBError', () => {
+
+            it('should throw internal', () => {
+
+                try {
+                    Avocat.rethrow(new DBError(
+                        {
+                            nativeError : 'NATIVE ERROR',
+                            client      : 'CLIENT'
+                        }
+                    ));
+                }
+                catch (error) {
+
+                    expect(error.isBoom).to.equal(true);
+                    expect(error.typeof.name).to.equal('internal');
+                    expect(error.output.statusCode).to.equal(500);
+                }
+
+            });
+
+            it('should return internal', () => {
+
+                const error = Avocat.rethrow(new DBError(
+                    {
+                        nativeError : 'NATIVE ERROR',
+                        client      : 'CLIENT'
+                    }
+                ), { return : true });
+
+                expect(error.isBoom).to.equal(true);
+                expect(error.typeof.name).to.equal('internal');
+                expect(error.output.statusCode).to.equal(500);
+            });
+        });
+
     });
-});
+})
+;
